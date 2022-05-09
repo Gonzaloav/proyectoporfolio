79e07027aff0880b2862489bff1c2508382ca82c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-09 13:48:10 +0200

    index

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,7 +1,20 @@
 import express from "express";
+import  { db } from "./sqlModels/db.mjs";
+import { getAllEtiquetasController, postEtiquetasController, 
+         putEtiquetasController, deleteEtiquetasController } 
+         from "./controllers/etiquetascontrollers.mjs";
+import { getAllFotosController, postFotosController, 
+         putFotosController, deleteFotosController } 
+        from "./controllers/fotoscontrollers.mjs";
+import { getAllGaleriasController, postGaleriasController, 
+         putGaleriasController, deleteGaleriasController } 
+         from "./controllers/galeriascontrollers.mjs";
 
 
-
+import { requestLog } from "./middeware/requestsLog.mjs"; 
+import { authMiddleware } from "./middeware/authorization.mjs";   
+import { errorsHandler } from "./middeware/errorsHandler.mjs";
+import { application } from "express";
 
 const PATH_FREFIX = "/api/v0.0"
 
@@ -11,14 +24,28 @@ try {
 
     app.use (requestLog);
 
-    app.get (PATH_FREFIX + "/porfolio/", );
-    app.post (PATH_FREFIX + "/porfolio/", );
-    app.put (PATH_FREFIX + "/porfolio/", );
-    app.delete (PATH_FREFIX + "/porfolio/", );
+
+    app.get (PATH_FREFIX + "/porfolio/",  authMiddleware, getAllEtiquetasController );
+    app.post (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, postEtiquetasController );
+    app.put (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, putEtiquetasController ); 
+    app.delete (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, deleteEtiquetasController );
+
+    app.get (PATH_FREFIX + "/porfolio/",  authMiddleware, getAllGaleriasController );
+    app.post (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, postGaleriasController );
+    app.put (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, putGaleriasController ); 
+    app.delete (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, deleteGaleriasController );
+   
+    app.get (PATH_FREFIX + "/porfolio/",  authMiddleware, getAllFotosController );
+    app.post (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, postFotosController );
+    app.put (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, putFotosController ); 
+    app.delete (PATH_FREFIX + "/porfolio/", jsonParser, authMiddleware, deleteFotosController );
+    
+    
+    app.use (errorsHandler);
 
     app.listen (process.env.PORT || 3000, ()=>{
-        console.log ("Express running...");
+        console.log ("Express funcionando...");
     });
 }   catch (err) {
-        console.error ("Error starting service:", err);
+        console.error ("Error del servicio:", err);
 }
