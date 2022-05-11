08b41547679736814f20353255cc7186a1a724e8
Gonzaloav <Gonzaloav@protonmail.com>
2022-05-11 18:10:04 +0200

    autorizacion error corregido

--- backend/src/middeware/authorization.mjs
+++ backend/src/middeware/authorization.mjs
@@ -1,39 +1,37 @@
-import { db } from "../models/db.mjs";
-import { authenticateUserSQL } from "../models/usersModels.mjs";
+import { db } from "../sqlModels/db.mjs";
+import { authenticateUserSQL } from "../sqlModels/usuarioSQL/userSqlModels.mjs";
 
-function decodeAuthBasic (headerContent) {
-    try {
-        const [ method, token ] = headerContent.split(" ");
-        const tokenString = atob(token);
-        const [ username, password ] = tokenString.split(":");
-        return { method, username, password }
-    } catch (error) {
-        throw "Autenticación mal formada";
-    }
+function decodeAuthBasic(headerContent) {
+  try {
+    const [method, token] = headerContent.split(" ");
+    const tokenString = atob(token);
+    const [username, password] = tokenString.split(":");
+    return { method, username, password };
+  } catch (error) {
+    throw "Autenticación mal formada";
+  }
 }
 
-export function authMiddleware( request, response, next ) {
-    try {
-        const { method, username, password } = decodeAuthBasic(request.headers.authorization);
+export function authMiddleware(request, response, next) {
+  try {
+    const { method, username, password } = decodeAuthBasic(
+      request.headers.authorization
+    );
 
-        if ( method != "Basic" ) throw "Método de autorización no válido. Use Básico, en su lugar."
-    
-        db.get(
-            authenticateUserSQL, [username, password],
-            (err, data) => {
-                if (err) throw err
-                if (data) {
-    // Guardar los datos que se van a ir resolviendo en los middeware. Linea 27
-                    response.locals.authorization = { name: data.name, id: data.id };
-                    next();
-                }
-                else throw "Error en la autorización."
-            }
-        )
+    if (method != "Basic")
+      throw "Método de autorización no válido. Use Básico, en su lugar.";
 
-    } catch (err) {
-        console.error(err);
-        response.sendStatus(401)
-        return;
-    }
+    db.get(authenticateUserSQL, [username, password], (err, data) => {
+      if (err) throw err;
+      if (data) {
+        // Guardar los datos que se van a ir resolviendo en los middeware. Linea 28
+        response.locals.authorization = { name: data.name, id: data.id };
+        next();
+      } else throw "Error en la autorización.";
+    });
+  } catch (err) {
+    console.error(err);
+    response.sendStatus(401);
+    return;
+  }
 }
--- backend/src/sqlModels/usuarioSQL/userSqlModels.mjs
+++ backend/src/sqlModels/usuarioSQL/userSqlModels.mjs
@@ -1,14 +1,11 @@
+export const getAllUsersSQL = `SELECT id, name FROM users WHERE name=? AND password=?`;
 
+export const postUsersSQL = `INSERT INTO users ( users_id, name, password ) VALUES (?, ?, ?)`;
 
+export const putUsersSQl = `UPDATE users SET users_id, = ?, password = ?  WHERE name = ? AND user_id = ?`;
 
-export const getAllUsersSQL = 
-`SELECT id, name FROM users WHERE name=? AND password=?`;
+export const deleteUsersSQL = `DELETE FROM Fotos WHERE name = ? AND user_id = ?`;
 
-export const postUsersSQL = 
-`INSERT INTO users ( users_id, name, password ) VALUES (?, ?, ?)`;
+export const authenticateUserSQL = `SELECT id, name FROM users WHERE name=? AND password=?`;
 
-export const putUsersSQl = 
-`UPDATE users SET users_id, = ?, password = ?  WHERE name = ? AND user_id = ?`;
-
-export const deleteUsersSQL = 
-`DELETE FROM Fotos WHERE name = ? AND user_id = ?`;
+// export const addUserSQL = `INSERT INTO users(name, password) VALUES (?, ?)`;
