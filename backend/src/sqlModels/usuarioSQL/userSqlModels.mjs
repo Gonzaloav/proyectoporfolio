9b7156fc8e3eeb7cfd3887ad921f06803421cbf8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-11 13:05:25 +0200

    usuario SQL

--- backend/src/sqlModels/usuarioSQL/userSqlModels.mjs
+++ backend/src/sqlModels/usuarioSQL/userSqlModels.mjs
@@ -12,9 +12,3 @@ export const putUsersSQl =
 
 export const deleteUsersSQL = 
 `DELETE FROM Fotos WHERE name = ? AND user_id = ?`;
-
-export const authenticateUserSQL = 
-`SELECT id, name FROM users WHERE name=? AND password=?`;
-
-export const addUserSQL = 
-`INSERT INTO users(name, password) VALUES (?, ?)`;
