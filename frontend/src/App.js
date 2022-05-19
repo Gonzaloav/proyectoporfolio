44fa60f1727739c5cd97d07a3979823ff49cf180
Gonzaloav <gonzaloav@protonmail.com>
2022-05-19 15:48:59 +0200

    prueba negativa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,27 @@
-
-import './App.css';
-import { useState } from 'react/cjs/react.production.min';
+import "./App.css";
+import { useState } from "react/cjs/react.production.min";
 
 function App() {
-
-  const [imagen, setImagen] = useState ([]);
+  const [imagen, setImagen] = useState([]);
 
   function atrasHandler(event) {
     setImagen(event.target.value);
   }
 
   function adelanteHandler(event) {
-    setImagen(event.target.value);  
+    setImagen(event.target.value);
   }
-
 
   return (
     <div>
-      <button onClick = {atrasHandler} value = "<"> </button>
-      <imagen alt = "Tortugas_1" src = "./components/fauna/Tortugas_rune_21.jpeg"/>
-      
-      <button onClick = {adelanteHandler} value = ">"> </button>
+      <button onClick={atrasHandler} value="<">
+        {" "}
+      </button>
+      <imagen alt="Tortugas_1" src="./components/fauna/Tortugas_rune_21.jpeg" />
+
+      <button onClick={adelanteHandler} value=">">
+        {" "}
+      </button>
     </div>
   );
 }
