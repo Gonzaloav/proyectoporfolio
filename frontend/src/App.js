df35cfc53c5c114f680b11e8ccbbe2760fc7f076
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-27 08:48:40 +0200

    varios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,18 @@
 
-import { useState } from "react";
-import  avanzar  from "./components/Imagenes/botones/flechaadelante.png";
-import  atrasar  from "./components/Imagenes/botones/flechaatras.png";
+import { useState, useEffect } from "react";
+
+import proporcionfoto  from "./components/proporcionfoto/proporcionfoto.mjs";
 
 
 function App() {
 
 
-  const [flecha, setFlecha] = useState([]);
+
   
-  // función flecha ">" o "<"
-  function flechaHandler(event) {
-    setFlecha (event.target.value);
-  }
- // función flecha anterior "<"
- function restaHandler(event) {
-    flechaHandler = (setFlecha --);
-  } 
-  // función flecha posterior ">"
-  function sumaHandler(event) {
-    flechaHandler = (setFlecha ++);  
-  }
 
   return (
     <div>
-      <button onClick={restaHandler} value = "-"> 
-          <img src= {atrasar}/> 
-      </button>
-      
-      <button onClick={sumaHandler} value = "+">  
-          <img src= {avanzar}/>
-      </button>
+        
     </div>
   );
 }
---
+++ text/index.js
@@ -0,0 +1,34 @@
+/**
+// sincrono
+const sizeOf = require('image-size')
+const dimensions = sizeOf('images/funny-cats.png')
+console.log(dimensions.width, dimensions.height)
+
+// Asincrono
+
+const sizeOf = require('image-size')
+sizeOf('images/funny-cats.png', 
+function (err, dimensions) {
+    console.log(dimensions.width, dimensions.height)
+  })
+
+
+  Multi-size =  Si el archivo de destino es un icono (.ico) o un cursor (.cur),
+ el ancho y alto serán los de la primera imagen encontrada.
+
+Una matriz de imágenes adicional está disponible y devuelve las dimensiones de
+ todas las imágenes disponibles 
+
+  const sizeOf = require('image-size')
+const images = sizeOf('images/multi-size.ico').images
+for (const dimensions of images) {
+  console.log(dimensions.width, dimensions.height)
+} 
+*/
+
+
+/*let imagen = new Image();
+imagen.src ="./img/Peru/Asaincas_13.jpg";
+console.log ("Anchura", imagen.naturalWidth);
+console.log ("Altura", imagen.naturalHeight);*/
+
