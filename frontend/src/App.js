66c9eaa73e49c154f0a0118d955777abaa07665d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-09 13:21:31 +0200

    frontend error prueba

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 //import "./App.css";
 //import Miniaturas from "./views/Miniaturas";
 //import Barrabotones from "./components/Barrabotones/Barrabotones";
-// import InicioLink from "./components/Link/Link";
-//import Routers from "./components/Rutas/Routers";
-//import { PorfolioLink, RoutersPorfolio} from "./components/Porfolio/Porfolio";
+
 import Carruselinico from "./components/carruseles/carruselinicio";
+
 
 
 function App() {
@@ -14,8 +13,6 @@ function App() {
      
       <Carruselinico/>
 
-
-     
     </div>
   );
 }
